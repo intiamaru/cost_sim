043effeef19d294965e803fa12b88f6866a1b26e
jboneu <jorgeboneu@gmail.com>
2014-06-13 22:29:44 -0500

    Tornado server bootstrap

--- cost_sim.py
+++ cost_sim.py
@@ -52,11 +52,12 @@ def feed_variables(file_name):
 
 def object_to_text_table(table):
     text_table = ''
-    column_names = table[0].keys()
-    text_table = text_table + '|' + '\t\t|'.join(column_names) + '\t\t|\n'
-    for row in table:
-        row_str = map(str,row.values())
-        text_table = text_table + '|' + '\t\t|'.join(row_str) + '\t\t|\n'
+    if len(table) > 0:
+        column_names = table[0].keys()
+        text_table = text_table + '|' + '\t\t|'.join(column_names) + '\t\t|\n'
+        for row in table:
+            row_str = map(str,row.values())
+            text_table = text_table + '|' + '\t\t|'.join(row_str) + '\t\t|\n'
     return text_table    
 
 
@@ -110,89 +111,90 @@ def process_simulation(year, batch_list=None):
     expenses = []
 
     f = open('output.txt','w')
-    sys.stdout = f
+    #sys.stdout = f
 
     #prepare data
     pa = unify(a,'batch')
     pb = unify(b,'batch')
     pc = unify(c,'batch')
     pd = unify(d,'batch')
-
+    
     for batch in pa:
-        batch_cost = pa[batch][0]['amount']
+        if (batch in batch_list):
+            batch_cost = pa[batch][0]['amount']
 
-        item_totals = []
-        for item in pb[batch]:
-            try:
-                item_totals.append(item['units'] * item['value'])
-            except TypeError:
-                print "Tipos incorrectos, revise " + item['units'] +"-"+ item['value']
-        oc_cost = sum(item_totals)
+            item_totals = []
+            for item in pb[batch]:
+                try:
+                    item_totals.append(item['units'] * item['value'])
+                except TypeError:
+                    print "Tipos incorrectos, revise " + item['units'] +"-"+ item['value']
+            oc_cost = sum(item_totals)
 
-        #obtain unit cost
-        elem_info = {}
-        for item in pb[batch]:
-            item['factor'] = item['units'] * item['value'] / oc_cost
-            item['cost'] = batch_cost * item['factor']
-            item['unit_cost'] = item['cost'] / item['units']
-            elem_info[item['name']] = item
-            
-        #element transformations
-        if batch in pc:
-            for item in pc[batch]:
-                origin_unit_cost = elem_info[item['name_from']]['unit_cost']
-                output_cost = origin_unit_cost * item['units_from']
-                output_unit_cost = output_cost / item['units_to']
-                new_item = {'name': item['name_to'], 'cost': output_cost,
-                            'unit_cost': output_unit_cost, 'units': item['units_to']}
-                elem_info[item['name_to']] = new_item
-
-
-        result_elem = {}
-        total_sales = []
-        total_sales_cost = []
-        for item in pd[batch]:
-            if item['year'] == year :
-                unit_cost = elem_info[item['name']]['unit_cost']
-                sales_cost = item['units'] * unit_cost
-                sales_value = item['units'] * item['price']
-                profit = sales_value - sales_cost
-                item['sales_value'] = sales_value
-                item['sales_cost'] = sales_cost
-                item['profit'] = profit
-                total_sales.append(sales_value)
-                total_sales_cost.append(sales_cost)
-                #print str(item['name']) + ": "
-                #print 'Venta: ' + str(sales_value)
-                #print 'Costo Venta: ' + str(sales_cost)
-                #print 'Utilidad: ' + str(profit)
+            #obtain unit cost
+            elem_info = {}
+            for item in pb[batch]:
+                item['factor'] = item['units'] * item['value'] / oc_cost
+                item['cost'] = batch_cost * item['factor']
+                item['unit_cost'] = item['cost'] / item['units']
+                elem_info[item['name']] = item
                 
-        result_elem['year'] = year
-        result_elem['total_sales'] = sum(total_sales)
-        result_elem['total_sales_cost'] = sum(total_sales_cost)
-        result_elem['final_profit'] = sum(total_sales) - sum(total_sales_cost)
-        final_profit = sum(total_sales) - sum(total_sales_cost)
-        result_elem['profit_percent'] = final_profit / sum(total_sales) * 100
+            #element transformations
+            if batch in pc:
+                for item in pc[batch]:
+                    origin_unit_cost = elem_info[item['name_from']]['unit_cost']
+                    print 'OUC ' + origin_unit_cost
+                    output_cost = origin_unit_cost * item['units_from']
+                    output_unit_cost = output_cost / item['units_to']
+                    new_item = {'name': item['name_to'], 'cost': output_cost,
+                                'unit_cost': output_unit_cost, 'units': item['units_to']}
+                    elem_info[item['name_to']] = new_item
 
-        print '==================================='
-        print 'R. Resultados (' + str(year) + '-' + str(batch) +'): '
-        print 'Vent Total: ' + str(sum(total_sales))
-        print 'Costo Venta Total: ' + str(sum(total_sales_cost))        
-        print 'Utilidad: ' + str(final_profit)
-        print 'Porcentaje Utilidad: ' + str( final_profit / sum(total_sales) * 100 )
-        process_result.append(result_elem)
+            #print elem_info
+
+            result_elem = {}
+            total_sales = []
+            total_sales_cost = []
+            for item in pd[batch]:
+                if item['year'] == year :
+                    unit_cost = elem_info[item['name']]['unit_cost']
+                    sales_cost = item['units'] * unit_cost
+                    sales_value = item['units'] * item['price']
+                    profit = sales_value - sales_cost
+                    item['sales_value'] = sales_value
+                    item['sales_cost'] = sales_cost
+                    item['profit'] = profit
+                    item['perc_profit'] = str(round(profit / sales_value * 100,2)) + '%'
+                    total_sales.append(sales_value)
+                    total_sales_cost.append(sales_cost)
+                    #print str(item['name']) + ':: Venta: ' + str(sales_value) + ' Costo Venta: ' + str(sales_cost) + ' Prct: ' + str(profit)
+                    
+            result_elem['year'] = year
+            result_elem['total_sales'] = sum(total_sales)
+            result_elem['total_sales_cost'] = sum(total_sales_cost)
+            result_elem['final_profit'] = sum(total_sales) - sum(total_sales_cost)
+            final_profit = sum(total_sales) - sum(total_sales_cost)
+            result_elem['profit_percent'] = final_profit / sum(total_sales) * 100
+
+            print '==================================='
+            print 'R. Resultados (' + str(year) + '-' + str(batch) +'): '
+            print 'Vent Total: ' + str(sum(total_sales))
+            print 'Costo Venta Total: ' + str(sum(total_sales_cost))        
+            print 'Utilidad: ' + str(final_profit)
+            print 'Porcentaje Utilidad: ' + str( final_profit / sum(total_sales) * 100 )
+            process_result.append(result_elem)
 
     print ""
-    print "Resultados del Proceso"
+    print "!=================Resultados del Proceso===============================!"
     print object_to_text_table(process_result)
-    f.close()
+    #f.close()
     
 var_dict = feed_variables('reporte.txt')
 a = var_dict['a']
 b = var_dict['b']
 c = var_dict['c']
 d = var_dict['d']
-process_simulation(2013)
+process_simulation(2013,['1003B'])
 
 #TDD TEST DRIVEN DEVELOPMENT
 def test1():
